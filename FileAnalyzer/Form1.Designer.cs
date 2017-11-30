3e4f7b8ceaa33f2bc0d44555b47c54fdac5600be
Viktor <viktor.melnic@gmail.com>
2017-11-30 23:50:03 +0200

    initial commit

---
+++ FileAnalyzer/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FileAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.maxLength = new System.Windows.Forms.Label();
+            this.maxLenText = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1198, 484);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // maxLength
+            // 
+            this.maxLength.AutoSize = true;
+            this.maxLength.Location = new System.Drawing.Point(18, 525);
+            this.maxLength.Name = "maxLength";
+            this.maxLength.Size = new System.Drawing.Size(63, 13);
+            this.maxLength.TabIndex = 1;
+            this.maxLength.Text = "Max Length";
+            // 
+            // maxLenText
+            // 
+            this.maxLenText.Location = new System.Drawing.Point(87, 522);
+            this.maxLenText.Name = "maxLenText";
+            this.maxLenText.ReadOnly = true;
+            this.maxLenText.Size = new System.Drawing.Size(100, 20);
+            this.maxLenText.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 548);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1198, 118);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 678);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.maxLenText);
+            this.Controls.Add(this.maxLength);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label maxLength;
+        private System.Windows.Forms.TextBox maxLenText;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ FileAnalyzer/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+
+
+namespace FileAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            listView1.Scrollable = true;
+            listView1.View = View.Details;
+            ColumnHeader header = new ColumnHeader();
+
+            header.Text = "File Names";
+            
+            listView1.Columns.Add(header);
+
+            List<String> fileList = Directory.EnumerateFiles(@"C:\Downloads").Select(x => Path.GetFileName(x)).ToList();
+            
+
+            
+            IEnumerable<char> distinctSymbols = fileList.
+                Aggregate<String>((x, y)=>x+y).
+                ToCharArray().
+                Distinct().
+                Where(x => !Char.IsLetterOrDigit(x)).
+                OrderByDescending(x=>x);
+            List<string[]> splitNames = fileList.Select(x => x.Split(distinctSymbols.ToArray<char>(), StringSplitOptions.RemoveEmptyEntries)).ToList();
+            HashSet<string> vocab = new HashSet<string>();
+            
+            foreach (string[] tokens in splitNames)
+            {
+                foreach (string token in tokens)
+                {
+                    vocab.Add(token);
+                }
+            }
+            List<string> index = vocab.OrderBy(x => x).ToList();
+            List<List<string>> parsedNames = splitNames.Select(x => x.Select(y => index.FindIndex(z=>z==y).ToString()).ToList()).ToList();
+            //todo: boolean index
+            richTextBox1.Text += new string(distinctSymbols.ToArray());
+            parsedNames.ForEach(x => listView1.Items.Add(x.Aggregate((a, b)=>a+" "+b)));
+            header.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            string maxLength = fileList.Max(x => x.Length).ToString();
+            maxLenText.Text = maxLength;
+            Matrix<double> m = Matrix<double>.Build.Random(10, 10);
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
